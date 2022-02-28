241f5d1052cc19e4840ad77d03587a3ad8ad1ef9
Balasubramaniam <60618007+Bala1008@users.noreply.github.com>
2022-02-28 12:04:57 +0530

    Projects page integrated

--- lib/Projects/datamod.dart
+++
@@ -1,5 +0,0 @@
-class Datamodel{
-  final List<String> subtopics;
-  final String topics;
-  Datamodel(this.subtopics,this.topics);
-}
--- lib/Projects/postClass.dart
+++ lib/Projects/postClass.dart
@@ -1,8 +1,17 @@
 class Post {
   final String title;
+  final String description;
   final String domain;
   final List<String> tags;
-  final String description;
   final String imageurl;
-  Post(this.title, this.domain, this.tags, this.description, this.imageurl);
+  final String gitlink;
+  final int time;
+  Post(
+      {required this.title,
+      required this.domain,
+      required this.tags,
+      required this.description,
+      required this.imageurl,
+      required this.gitlink,
+      required this.time});
 }
